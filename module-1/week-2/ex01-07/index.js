4aa622f10bdce715e132e016119a4df5daeb406b
Matheus <halkner77@hotmail.com>
2022-07-10 00:26:46 -0300

    Exercício 7finalizado

--- module-1/week-2/ex01-07/index.js
+++ module-1/week-2/ex01-07/index.js
@@ -7,3 +7,16 @@ function abreFechaMenu(elemento){
         dropdown.style.display = "none";
     }
 }
+
+function enviarForm(n, em, desc){
+    let nome = n.value
+    let email = em.value
+    let descricao = desc.value
+
+    console.log(`
+    Nome: ${nome}
+    E-mail: ${email}
+    Descrição: ${descricao}`);
+
+    alert("Obrigado por entrar em contato, retornamos em breve! :)")
+}
