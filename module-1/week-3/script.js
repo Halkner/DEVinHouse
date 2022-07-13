67f22b67d4d55dd86c256ce938f0239b2aaebe3c
Matheus Adriano Martins <halkner77@hotmail.com>
2022-07-13 17:43:14 -0300

    [M01S03] EX 02 finalizado.

--- module-1/week-3/script.js
+++ module-1/week-3/script.js
@@ -2,8 +2,28 @@
 
 var listaNumeros = [ 2, 3, 9, 12, 16, 55, 90, 78, 17 ];
 
-listaNumeros.forEach(function(num, i){
+listaNumeros.forEach(function(undefined, i){
     listaNumeros[i] *= 2;
 });
 
 console.log(listaNumeros);
+
+// [M01S03] EX02
+
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+];
+
+for (var i = 0; i < listaConvidados.length; i++){
+    console.log(`${listaConvidados[i].nome} ${listaConvidados[i].sobrenome}`);
+    document.getElementById("lista").innerHTML += `<li>${listaConvidados[i].nome} ${listaConvidados[i].sobrenome}</li>`
+};
