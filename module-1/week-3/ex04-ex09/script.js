c8e5063083b8250a4aedb48b861a37f15ffb8c16
Matheus Adriano Martins <halkner77@hotmail.com>
2022-07-14 17:49:56 -0300

    [M01S03] EX 08 finalizado.

---
+++ module-1/week-3/ex04-ex09/script.js
@@ -0,0 +1,76 @@
+// [M01S03] EX04 -----------------------------------
+
+function pessoaConvidada(nome, sobrenome, setor, idade){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.setor = setor;
+    this.idade = idade;
+}
+
+let pessoa1 = new pessoaConvidada("Cristiana", "Elias", "Camarote", 10);
+let pessoa2 = new pessoaConvidada("Geovana", "Antunes", "Pista", 20);
+let pessoa3 = new pessoaConvidada("Elizandra", "Gomes", "Arquibancada", 17);
+let pessoa4 = new pessoaConvidada("André","de Lima", "Arquibancada", 30);
+let pessoa5 = new pessoaConvidada("Fábio", "Mariano", "Pista", 18);
+let pessoa6 = new pessoaConvidada("Adilson", "Elias", "Camarote", 45);
+
+// [M01S03] EX05 -----------------------------------
+
+var listaDeConvidados = [pessoa1, pessoa2, pessoa3, pessoa4, pessoa5, pessoa6];
+
+// [M01S03] EX06 -----------------------------------
+
+function liberarBebidas(listaConvidados){
+    listaVazia = [];
+    for (const i in listaConvidados){
+        convidado = listaConvidados[i];
+        if (convidado.idade > 17){
+            convidado["openBar"] = true;
+        }else{
+            convidado["openBar"] = false;
+        }
+        listaVazia.push(convidado);
+    }
+    return listaVazia;
+}
+
+var convidadosComBebidasProcessada = liberarBebidas(listaDeConvidados);
+
+// [M01S03] EX07 -----------------------------------
+
+function separarCamarote(listaConvidados){
+    let listaConvidadosFiltrada = listaConvidados.filter(convidado => convidado.setor === "Camarote");
+    return listaConvidadosFiltrada;
+}
+
+function separarPista(listaConvidados){
+    let listaConvidadosFitlrada = listaConvidados.filter(convidado => convidado.setor === "Pista");
+    return listaConvidadosFitlrada
+}
+
+function separarArquibancada(listaConvidados){
+    let listaConvidadosFitlrada = listaConvidados.filter(convidado => convidado.setor === "Arquibancada");
+    return listaConvidadosFitlrada
+}
+
+let convidadosCamarote = separarCamarote(convidadosComBebidasProcessada);
+let convidadosPista = separarPista(convidadosComBebidasProcessada);
+let convidadosArquibancada = separarArquibancada(convidadosComBebidasProcessada);
+
+// [M01S03] EX08 -----------------------------------
+
+let listaCamarote = document.getElementById("listaCamarote");
+let listaArquibancada = document.getElementById("listaArquibancada");
+let listaPista = document.getElementById("listaPista");
+
+Object.keys(convidadosCamarote).forEach(item => {
+    listaCamarote.innerHTML += `<li>${convidadosCamarote[item].nome} ${convidadosCamarote[item].sobrenome}</li>`
+});
+
+Object.keys(convidadosArquibancada).forEach(item => {
+    listaArquibancada.innerHTML += `<li>${convidadosArquibancada[item].nome} ${convidadosArquibancada[item].sobrenome}</li>`
+});
+
+Object.keys(convidadosPista).forEach(item => {
+    listaPista.innerHTML += `<li>${convidadosPista[item].nome} ${convidadosPista[item].sobrenome}</li>`
+});
--- module-1/week-3/script.js
+++ module-1/week-3/script.js
@@ -1,121 +1,54 @@
-// // [M01S03] EX01 -----------------------------------
+// [M01S03] EX01 -----------------------------------
 
-// var listaNumeros = [ 2, 3, 9, 12, 16, 55, 90, 78, 17 ];
+var listaNumeros = [ 2, 3, 9, 12, 16, 55, 90, 78, 17 ];
 
-// listaNumeros.forEach(function(undefined, i){
-//     listaNumeros[i] *= 2;
-// });
+listaNumeros.forEach(function(undefined, i){
+    listaNumeros[i] *= 2;
+});
 
-// console.log(listaNumeros);
+console.log(listaNumeros);
 
-// // [M01S03] EX02 -----------------------------------
+// [M01S03] EX02 -----------------------------------
 
-// var listaConvidados = [
-//     { nome: "José", sobrenome: "Carlos" },
-//     { nome: "Alessandro", sobrenome: "Viana" },
-//     { nome: "Paula", sobrenome: "Souza" },
-//     { nome: "Cristian", sobrenome: "Schimit" },
-//     { nome: "Beatriz", sobrenome: "Viana" },
-//     { nome: "Fernanda", sobrenome: "Silveira" },
-//     { nome: "Cláudia", sobrenome: "Torres" },
-//     { nome: "Augusto", sobrenome: "Cesar" },
-//     { nome: "Noemi", sobrenome: "Nakamura" },
-//     { nome: "Pedro", sobrenome: "Lobo" },
-// ];
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+];
 
-// for (var i = 0; i < listaConvidados.length; i++){
-//     document.getElementById("lista").innerHTML += `<li>${listaConvidados[i].nome} ${listaConvidados[i].sobrenome}</li>`
-// };
+for (var i = 0; i < listaConvidados.length; i++){
+    document.getElementById("lista").innerHTML += `<li>${listaConvidados[i].nome} ${listaConvidados[i].sobrenome}</li>`
+};
 
-// // [M01S03] EX03 -----------------------------------
+// [M01S03] EX03 -----------------------------------
 
 
-// n1 = prompt("Qual o primeiro número?");
-// n2 = prompt("Qual o segundo número?");
-// operacao = prompt("Qual operação matemática deseja realizar? [+, -, * ou /]");
+n1 = prompt("Qual o primeiro número?");
+n2 = prompt("Qual o segundo número?");
+operacao = prompt("Qual operação matemática deseja realizar? [+, -, * ou /]");
 
-// switch(operacao){
-//     case "+":
-//         let soma = parseInt(n1) + parseInt(n2);
-//         console.log(`O resultado da soma é: ${soma}.`);
-//         break;
-//     case "-":
-//         let subtrai = parseInt(n1) - parseInt(n2);
-//         console.log(`O resultado da subtração é: ${subtrai}.`);
-//         break;
-//     case "*":
-//         let multiplica = parseInt(n1) * parseInt(n2);
-//         console.log(`O resultado da multiplicação é: ${multiplica}.`);
-//         break;
-//     case "/":
-//         let divide = parseInt(n1) / parseInt(n2);
-//         console.log(`O resultado da divisão é: ${divide}.`);
-//         break;
-// }
-
-// [M01S03] EX04 -----------------------------------
-
-function pessoaConvidada(nome, sobrenome, setor, idade){
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-    this.setor = setor;
-    this.idade = idade;
+switch(operacao){
+    case "+":
+        let soma = parseInt(n1) + parseInt(n2);
+        console.log(`O resultado da soma é: ${soma}.`);
+        break;
+    case "-":
+        let subtrai = parseInt(n1) - parseInt(n2);
+        console.log(`O resultado da subtração é: ${subtrai}.`);
+        break;
+    case "*":
+        let multiplica = parseInt(n1) * parseInt(n2);
+        console.log(`O resultado da multiplicação é: ${multiplica}.`);
+        break;
+    case "/":
+        let divide = parseInt(n1) / parseInt(n2);
+        console.log(`O resultado da divisão é: ${divide}.`);
+        break;
 }
-
-let pessoa1 = new pessoaConvidada("Amanda", "da Silva", "Camarote", 10);
-let pessoa2 = new pessoaConvidada("Geovana", "Antunes", "Pista", 20);
-let pessoa3 = new pessoaConvidada("Elizandra", "Gomes", "Arquibancada", 17);
-let pessoa4 = new pessoaConvidada("André","de Lima", "Arquibancada", 30);
-let pessoa5 = new pessoaConvidada("Fábio", "Mariano", "Pista", 18);
-let pessoa6 = new pessoaConvidada("Adilson", "Elias", "Camarote", 45);
-
-// [M01S03] EX05 -----------------------------------
-
-var listaDeConvidados = [pessoa1, pessoa2, pessoa3, pessoa4, pessoa5, pessoa6];
-
-// [M01S03] EX06 -----------------------------------
-
-function liberarBebidas(listaConvidados){
-    listaVazia = [];
-    for (const i in listaConvidados){
-        convidado = listaConvidados[i];
-        if (convidado.idade > 17){
-            convidado["openBar"] = true;
-        }else{
-            convidado["openBar"] = false;
-        }
-        listaVazia.push(convidado);
-    }
-    return listaVazia;
-}
-
-var convidadosComBebidasProcessada = liberarBebidas(listaDeConvidados);
-// console.log(convidadosComBebidasProcessada);
-
-// [M01S03] EX07 -----------------------------------
-
-function separarCamarote(listaConvidados){
-    let listaConvidadosFiltrada = listaConvidados.filter(convidado => convidado.setor === "Camarote");
-    return listaConvidadosFiltrada;
-}
-
-function separarPista(listaConvidados){
-    let listaConvidadosFitlrada = listaConvidados.filter(convidado => convidado.setor === "Pista");
-    return listaConvidadosFitlrada
-}
-
-function separarArquibancada(listaConvidados){
-    let listaConvidadosFitlrada = listaConvidados.filter(convidado => convidado.setor === "Arquibancada");
-    return listaConvidadosFitlrada
-}
-
-let listaCamarote = separarCamarote(convidadosComBebidasProcessada);
-let listaPista = separarPista(convidadosComBebidasProcessada);
-let listaArquibancada = separarArquibancada(convidadosComBebidasProcessada);
-
-// console.log("-- Lista Camarote --");
-// console.log(listaCamarote);
-// console.log("-- Lista Pista --");
-// console.log(listaPista);
-// console.log("-- Lista Arquibancada --");
-// console.log(listaArquibancada);
