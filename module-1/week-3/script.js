9cd7219acbe79712e9b402909c04b2c9a707f9c0
Matheus Adriano Martins <halkner77@hotmail.com>
2022-07-14 12:40:12 -0300

    [M01S03] EX 04 finalizado.

--- module-1/week-3/script.js
+++ module-1/week-3/script.js
@@ -27,29 +27,46 @@
 //     document.getElementById("lista").innerHTML += `<li>${listaConvidados[i].nome} ${listaConvidados[i].sobrenome}</li>`
 // };
 
-// [M01S03] EX03 -----------------------------------
+// // [M01S03] EX03 -----------------------------------
 
 
-n1 = prompt("Qual o primeiro número?");
-n2 = prompt("Qual o segundo número?");
-operacao = prompt("Qual operação matemática deseja realizar? [+, -, * ou /]");
+// n1 = prompt("Qual o primeiro número?");
+// n2 = prompt("Qual o segundo número?");
+// operacao = prompt("Qual operação matemática deseja realizar? [+, -, * ou /]");
 
-switch(operacao){
-    case "+":
-        let soma = parseInt(n1) + parseInt(n2);
-        console.log(`O resultado da soma é: ${soma}.`);
-        break;
-    case "-":
-        let subtrai = parseInt(n1) - parseInt(n2);
-        console.log(`O resultado da subtração é: ${subtrai}.`);
-        break;
-    case "*":
-        let multiplica = parseInt(n1) * parseInt(n2);
-        console.log(`O resultado da multiplicação é: ${multiplica}.`);
-        break;
-    case "/":
-        let divide = parseInt(n1) / parseInt(n2);
-        console.log(`O resultado da divisão é: ${divide}.`);
-        break;
+// switch(operacao){
+//     case "+":
+//         let soma = parseInt(n1) + parseInt(n2);
+//         console.log(`O resultado da soma é: ${soma}.`);
+//         break;
+//     case "-":
+//         let subtrai = parseInt(n1) - parseInt(n2);
+//         console.log(`O resultado da subtração é: ${subtrai}.`);
+//         break;
+//     case "*":
+//         let multiplica = parseInt(n1) * parseInt(n2);
+//         console.log(`O resultado da multiplicação é: ${multiplica}.`);
+//         break;
+//     case "/":
+//         let divide = parseInt(n1) / parseInt(n2);
+//         console.log(`O resultado da divisão é: ${divide}.`);
+//         break;
+// }
+
+// [M01S03] EX04 -----------------------------------
+
+function pessoaConvidada(nome, sobrenome, setor, idade){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.setor = setor;
+    this.idade = idade;
 }
 
+let pessoa1 = new pessoaConvidada("Amanda", "da Silva", "Camarote", 10);
+let pessoa2 = new pessoaConvidada("Geovana", "Antunes", "Pista", 20);
+let pessoa3 = new pessoaConvidada("Elizandra", "Gomes", "Arquibancada", 17);
+let pessoa4 = new pessoaConvidada("André","de Lima", "Arquibancada", 30);
+let pessoa5 = new pessoaConvidada("Fábio", "Mariano", "Pista", 18);
+let pessoa6 = new pessoaConvidada("Adilson", "Elias", "Camarote", 45);
+
+console.log(pessoa1);
