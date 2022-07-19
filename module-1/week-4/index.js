f0eaa934ee28480e4c6c963e6d0867f2823f7043
Matheus Adriano Martins <halkner77@hotmail.com>
2022-07-19 11:33:06 -0300

    [M01S04] EX 02 finalizado.

---
+++ module-1/week-4/index.js
@@ -0,0 +1,17 @@
+const contasClientes = [
+    {
+    id: 1,
+    nome: 'Cristiana',
+    saldo: 51630,
+    },
+    {
+    id: 2,
+    nome: 'Matheus',
+    saldo: 45970,
+    },
+    {
+    id: 3,
+    nome: 'Jaderson',
+    saldo: 27350,
+    },
+];
