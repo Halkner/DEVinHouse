21916a1fa988064ccbccb69d1bb080cd4f401099
Matheus Adriano Martins <halkner77@hotmail.com>
2022-07-19 15:14:53 -0300

    [M01S04] EX 04 finalizado.

--- module-1/week-4/index.js
+++ module-1/week-4/index.js
@@ -1,3 +1,4 @@
+// Clientes cadastrados
 const contasClientes = [
     {
     id: 1,
@@ -16,10 +17,52 @@ const contasClientes = [
     },
 ];
 
+// Atribuindo o select do dropdown a uma variável
 let selectClientes = document.getElementById("conta");
 
+// Função onload para passar os clientes do array para o dropdown
 function inputClientes(){
     for(let i = 0; i < contasClientes.length; i++){
         selectClientes.innerHTML += `<option>${contasClientes[i].nome}</option>`
+    }
+}
+
+// Função para descobrir o ID do cliente informado.
+function descobreID(cliente){
+    for(var chave in contasClientes){
+        if(contasClientes[chave].nome === cliente){
+            return contasClientes[chave].id;
+        }
+    }
+}
+
+// Função de submit do botão do form
+function enviarOperacao(){
+    let operacao = document.getElementById("operacao").value;
+    let valor = Number(document.getElementById("valor").value);
+    let cliente = selectClientes.value;
+    let id = descobreID(cliente);
+
+    switch(operacao){
+        case "sacar":
+            sacar(valor, id);
+            break;
+        case "depositar":
+
+    }
+}
+
+// Função de saque
+function sacar(valor, id){
+    let saldo = contasClientes[id-1].saldo;
+    
+    if (valor <= 0 || isNaN(valor)){
+        alert("Valor de saque inválido.");
+    }else if(valor > saldo){
+        alert(`Saldo insuficiente! Seu saldo é de: R$ ${saldo}`);
+    }else{
+        contasClientes[id-1].saldo -= valor
+        saldoAposSaque = saldo - valor;
+        alert(`Saque feito com sucesso! Foi sacado R$ ${valor} do seu saldo de R$ ${saldo}, ficando com R$ ${saldoAposSaque}. `);
     }
 }
