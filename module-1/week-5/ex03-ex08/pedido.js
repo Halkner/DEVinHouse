287ac71befbfc962e8bc0389684293f41b03259a
Matheus Adriano Martins <halkner77@hotmail.com>
2022-07-28 17:37:15 -0300

    Subindo pra fazer em casa

--- module-1/week-5/ex03-ex08/pedido.js
+++
@@ -1,9 +0,0 @@
-class Pedido{
-    constructor(numeroPedido, nomeCliente){
-        this.numeroPedido = numeroPedido;
-        this.nomeCliente = nomeCliente;
-        this.dataPedido = new Date().toLocaleDateString();
-        this.estaPago = false;
-        this.listaProdutos = [];
-    }
-}
--- module-1/week-5/ex03-ex08/produto.js
+++
@@ -1,8 +0,0 @@
-class Produto {
-    constructor(nome, preco, emEstoque, quantidade){
-        this.nome = nome;
-        this.preco = preco;
-        this.emEstoque = emEstoque;
-        this.quantidade = quantidade;
-    }
-}
