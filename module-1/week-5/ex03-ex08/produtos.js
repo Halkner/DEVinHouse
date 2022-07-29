223cb691047f8a08cb0d257b6b3ff53be4568c1a
Matheus <halkner77@hotmail.com>
2022-07-29 19:21:47 -0300

    [M01S05] EX 07 finalizado.

--- module-1/week-5/ex03-ex08/produtos.js
+++ module-1/week-5/ex03-ex08/produtos.js
@@ -24,19 +24,34 @@ class Pedido {
         this.listaProdutos.forEach((item) => {
             valorTotal += item['quantidade'] * item['preco'];
         });
-        return valorTotal;
+        return valorTotal.toFixed(2);
     }
 }
 
-const produto1 = new Produto("Batata", 8.99, true, 20);
-const produto2 = new Produto("Maçã", 4.89, true, 15);
+const produto1 = new Produto("Batata", 2.99, true, 10);
+const produto2 = new Produto("Maçã", 2.50, true, 5);
+const produto3 = new Produto("Shampoo", 23.49, true, 1);
+const produto4 = new Produto("Banana", 7.50, true, 2);
+const produto5 = new Produto("Creme de Barbear", 7.99, true, 1);
 
-const pedido1 = new Pedido(01, "Matheus");
+
+const pedido1 = new Pedido("20220001", "Cristiana");
 
 pedido1.adicionarProduto(produto1);
 pedido1.adicionarProduto(produto2);
+pedido1.adicionarProduto(produto4);
+
+const pedido2 = new Pedido("20220002", "Matheus");
+
+pedido2.adicionarProduto(produto3);
+pedido2.adicionarProduto(produto5);
+
 
 console.log(pedido1);
+console.log(pedido2);
 
 valorPedido1 = pedido1.calcularTotal();
-console.log(`Valor total do pedido: R$${valorPedido1}`);
+console.log(`Valor total do pedido nº 1: R$${valorPedido1}`);
+
+valorPedido2 = pedido2.calcularTotal();
+console.log(`Valor total do pedido nº 2: R$${valorPedido2}`);
