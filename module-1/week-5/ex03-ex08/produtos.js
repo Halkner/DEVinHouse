b40e7c3284062e12c5f15e153e4930678d852310
Matheus Adriano Martins <halkner77@hotmail.com>
2022-07-29 14:17:17 -0300

    [M01S05] EX 06 finalizado.

--- module-1/week-5/ex03-ex08/produtos.js
+++ module-1/week-5/ex03-ex08/produtos.js
@@ -18,11 +18,25 @@ class Pedido {
     adicionarProduto(objProduto){
         this.listaProdutos.push(objProduto);
     }
+
+    calcularTotal(){
+        let valorTotal = 0;
+        this.listaProdutos.forEach((item) => {
+            valorTotal += item['quantidade'] * item['preco'];
+        });
+        return valorTotal;
+    }
 }
 
 const produto1 = new Produto("Batata", 8.99, true, 20);
-console.log(produto1);
+const produto2 = new Produto("Maçã", 4.89, true, 15);
 
 const pedido1 = new Pedido(01, "Matheus");
+
 pedido1.adicionarProduto(produto1);
+pedido1.adicionarProduto(produto2);
+
 console.log(pedido1);
+
+valorPedido1 = pedido1.calcularTotal();
+console.log(`Valor total do pedido: R$${valorPedido1}`);
