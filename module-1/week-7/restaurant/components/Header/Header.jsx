39079c93353939ee58681c4f828519fb0e1d8ca4
Matheus <halkner77@hotmail.com>
2022-08-11 20:21:10 -0300

    [M01S07] EX 02 finalizado.

--- module-1/week-7/restaurant/components/Header/Header.jsx
+++ module-1/week-7/restaurant/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () =>{
     <header>
         <ul>
             <li className="logo"><img src="./public/assets/logo.png" alt="Logo Restaurante" /></li>
-            <li className="menu">Menu</li>
+            <li className="menu"><a href="#">Cardápio</a></li>
         </ul>
     </header>
     );
