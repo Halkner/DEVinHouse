3a9c3c39ccf3ed44e5b321ac5f44adb737c9bc17
Matheus Adriano Martins <halkner77@hotmail.com>
2022-08-12 16:51:46 -0300

    Subindo pra casa

---
+++ module-1/week-7/restaurant/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+export const Card = () => {
+    return (
+        <div className="card-container">
+            <div><img src="" alt="" /></div>
+
+            <ul>
+                <li>Nome</li>
+                <li>Descrição</li>
+                <li>Valor</li>
+                <li>Tempo de preparo</li>
+            </ul>
+
+        </div>
+    )
+}
--- module-1/week-7/restaurant/src/App.jsx
+++ module-1/week-7/restaurant/src/App.jsx
@@ -1,10 +1,12 @@
 import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Header/>
+      <main></main>
       <Footer/>
     </div>
   );
