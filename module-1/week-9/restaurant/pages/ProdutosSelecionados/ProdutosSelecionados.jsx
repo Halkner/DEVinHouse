a8266cdaab5c10b52f9c9295cf5325338cc7a392
Matheus <halkner77@hotmail.com>
2022-09-02 17:51:05 -0300

    Finalizado

--- module-1/week-9/restaurant/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
+++ module-1/week-9/restaurant/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
@@ -6,13 +6,15 @@ import {useSelect} from '../../hooks/useSelect'
 import styles from './ProdutosSelecionados.module.css'
 
 export const ProdutosSelecionados = () => {
-    const {selecionados} = useSelect();
+    const {selecionados, clearProducts} = useSelect();
 
     const total = selecionados.reduce((soma, item) => soma + item.valor, 0)
     
     return(
         <React.Fragment>
-            <h1 className={styles.prodSelectedTitle}>Produtos Selecionados</h1> 
+            <h1 className={styles.prodSelectedTitle}>Produtos Selecionados</h1>
+            <p className={styles.limpaProdutos}
+                onClick={() => clearProducts()}>Limpar produtos selecionados</p>
             <ListaCards produtos={selecionados}/>
             <p className={styles.total}>Valor total: {`R$ ${total.toFixed(2).replace('.', `,`)}`}</p>
         </React.Fragment>  
