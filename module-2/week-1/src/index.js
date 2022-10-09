128c6282c3223f64decc51e9b8c87e6bd0f95a50
Matheus <halkner77@hotmail.com>
2022-10-09 15:42:16 -0300

    Exercício 3 finalizado.

--- module-2/week-1/src/index.js
+++ module-2/week-1/src/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const {v4: uuidv4} = require('uuid');
 
@@ -71,6 +72,59 @@ app.post('/pizzas', (request, response) => {
     pizzas.push(newPizza);
 
     return response.status(201).json(newPizza);
+})
+
+
+// Orders
+
+let orders = [
+    {
+        id: uuidv4(),
+        name: "Matheus",
+        cpf: "10642496986",
+        address: "Rua Bertoldo Simão De Oliveira",
+        phone: "(48) 9 9152-5320",
+        payment: "Cartão de crédito",
+        obs: "Sem cebola",
+        pizza: [
+            {
+                "id": "8cc9ac4f-651b-4ba3-96c7-fcccf2457f74",
+                "name": "Pepperoni",
+                "description": "Pizza de pepperoni",
+                "price": "43,00",
+                "ingredients": [
+                    "Cheese",
+                    "Special Sauce",
+                    "Pepperoni"
+                ]
+            }
+        ]
+    }
+]
+
+
+app.get('/orders', (request, response) => {
+
+    return response.status(200).json(orders);
+})
+
+app.post('/orders', (request, response) => {
+    const {name, cpf, address, phone, payment, obs, pizza} = request.body;
+
+    const newOrder = {
+        id: uuidv4(),
+        name,
+        cpf,
+        address,
+        phone,
+        payment,
+        obs,
+        pizza
+    }
+
+    orders.push(newOrder);
+
+    return response.status(201).json(newOrder);
 })
 
 app.listen(3000, () => {
