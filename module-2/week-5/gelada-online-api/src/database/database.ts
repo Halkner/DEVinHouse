d71f060a31c0fa14ec1c723c0e3e81b879ac0f5c
Matheus global <halkner77@hotmail.com>
2022-11-21 21:03:32 -0300

    Exercício 3 finalizado.

--- module-2/week-5/gelada-online-api/src/database/database.ts
+++ module-2/week-5/gelada-online-api/src/database/database.ts
@@ -1,6 +1,24 @@
 import { Injectable } from "@nestjs/common";
+import { readFileSync, writeFileSync } from "fs";
+import { BeerType } from "src/cervejas/types/beer-type.enum";
 
 @Injectable()
 export class Database{
     private FILENAME = './src/database/beers.json';
+
+    public saveData(data: BeerType) {
+        const content = JSON.parse(readFileSync(this.FILENAME, 'utf-8'));
+
+        const updatedContent = [...content, data];
+
+        writeFileSync(this.FILENAME, JSON.stringify(updatedContent));
+
+        return data;
+    }
+
+    public loadData(){
+        const content = JSON.parse(readFileSync(this.FILENAME, 'utf-8'));
+
+        return content;
+    }
 }
